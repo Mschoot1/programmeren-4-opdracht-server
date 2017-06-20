1f8528ac84ab7f8170ca39cc52c4b3ddaede4768
Quoc Sy Luong <qsluong1@gmail.com>
2017-06-20 10:37:27 +0200

    Removed function decodeToken

--- auth/authentication.js
+++ auth/authentication.js
@@ -11,28 +11,6 @@ function encodeToken(customer_id) {
     return jwt.encode(payload, settings.secretKey);
 }
 
-function decodeToken(token, cb) {
-
-    try {
-        const payload = jwt.decode(token, settings.secretKey);
-
-        // Check if the token has expired. To do: Trigger issue in db ..
-        const now = moment().unix();
-
-        // Check if the token has expired
-        if (now > payload.exp) {
-            console.log('Token has expired.');
-        }
-
-        // Return
-        cb(null, payload);
-
-    } catch (err) {
-        cb(err, null);
-    }
-}
-
 module.exports = {
     encodeToken,
-    decodeToken
 };
