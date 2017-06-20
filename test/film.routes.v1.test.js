00e15146b505df091b0158ce71a7cd0206a680e1
Mschoot1 <marnix_vd_schoot@hotmail.com>
2017-06-20 07:42:54 +0200

    films endpoint test fixed

--- test/film.routes.v1.test.js
+++ test/film.routes.v1.test.js
@@ -7,15 +7,13 @@ chai.use(chaiHttp);
 
 describe('Film routes api v1', function () {
     it('returns an array on GET /api/v1/films', function (done) {
-        var count = 1;
-        var offset = 0;
         chai.request(server)
-            .get('/api/v1/films?offset=' + offset + '&count=' + count)
+            .get('/api/v1/films')
             .end(function (err, res) {
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.an('object');
-                res.body.should.have.property('result').that.is.an('array').that.is.length(1);
+                res.body.should.have.property('result').that.is.an('array').that.is.length(0);
                 done();
             });
     });
